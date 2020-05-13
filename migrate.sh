1b3c284fcce43c8924871c1bf7ac0809cc40299e
macinjoke <shunji.makino@gmail.com>
2020-05-13 17:40:26 +0900

    update for かいしゃ

--- migrate.sh
+++ migrate.sh
@@ -1,3 +1,6 @@
 ln -s ~/.ghq/github.com/macinjoke/my-git/gitconfig ~/.gitconfig
 ln -s ~/.ghq/github.com/macinjoke/my-git/gitignore ~/.gitignore
 ln -s ~/.ghq/github.com/macinjoke/my-git/tigrc ~/.tigrc
+
+# for dwango pc
+# ln -s ~/.ghq/github.com/macinjoke/my-git/gitconfig_dwango ~/.gitconfig_dwango
